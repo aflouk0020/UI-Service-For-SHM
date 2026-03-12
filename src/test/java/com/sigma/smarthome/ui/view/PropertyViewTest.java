1ca3109750df97f0a115cf488c0d96a672228a0d
Taha Aflouk <tahaaflouk94@gmail.com>
2026-03-12 22:59:07 +0000

    test: improve coverage for property service and property view

--- src/test/java/com/sigma/smarthome/ui/view/PropertyViewTest.java
+++ src/test/java/com/sigma/smarthome/ui/view/PropertyViewTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +34,12 @@ class PropertyViewTest {
     @BeforeEach
     void clearSession() {
         SessionManager.clearSession();
+    }
+    
+    private void invokePrivateMethod(Object target, String methodName) throws Exception {
+        var method = target.getClass().getDeclaredMethod(methodName);
+        method.setAccessible(true);
+        method.invoke(target);
     }
 
     @Test
@@ -434,5 +441,234 @@ class PropertyViewTest {
         Field field = target.getClass().getDeclaredField(fieldName);
         field.setAccessible(true);
         return (T) field.get(target);
+    }
+    
+    @Test
+    void handleAddProperty_readOnly_showsMessage() throws Exception {
+        SessionManager.startSession("token", "staff@test.com", "MAINTENANCE_STAFF");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+        Label messageLabel = getPrivateField(view, "messageLabel", Label.class);
+
+        invokePrivateMethod(view, "handleAddProperty");
+
+        assertTrue(messageLabel.isVisible());
+        assertEquals("Maintenance staff can view property information only.", messageLabel.getText());
+    }
+
+    @Test
+    void handleEditProperty_noSelection_showsMessage() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of(
+                new Property("1", "Athlone", "House", "manager1")
+        ));
+
+        PropertyView view = new PropertyView(fakeService);
+        Label messageLabel = getPrivateField(view, "messageLabel", Label.class);
+
+        invokePrivateMethod(view, "handleEditProperty");
+
+        assertTrue(messageLabel.isVisible());
+        assertEquals("Please select a property to edit.", messageLabel.getText());
+    }
+
+    @Test
+    void handleDeleteProperty_noSelection_showsMessage() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of(
+                new Property("1", "Athlone", "House", "manager1")
+        ));
+
+        PropertyView view = new PropertyView(fakeService);
+        Label messageLabel = getPrivateField(view, "messageLabel", Label.class);
+
+        invokePrivateMethod(view, "handleDeleteProperty");
+
+        assertTrue(messageLabel.isVisible());
+        assertEquals("Please select a property to delete.", messageLabel.getText());
+    }
+    
+    @Test
+    void propertyView_addAction_readOnlyUser_showsReadOnlyMessage() throws Exception {
+        SessionManager.startSession("token", "staff@test.com", "MAINTENANCE_STAFF");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method method = PropertyView.class.getDeclaredMethod("handleAddProperty");
+        method.setAccessible(true);
+        method.invoke(view);
+
+        Label messageLabel = getPrivateField(view, "messageLabel", Label.class);
+
+        assertEquals("Maintenance staff can view property information only.", messageLabel.getText());
+        assertTrue(messageLabel.isVisible());
+    }
+    
+    @Test
+    void propertyView_editAction_withoutSelection_showsMessage() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method method = PropertyView.class.getDeclaredMethod("handleEditProperty");
+        method.setAccessible(true);
+        method.invoke(view);
+
+        Label messageLabel = getPrivateField(view, "messageLabel", Label.class);
+
+        assertEquals("Please select a property to edit.", messageLabel.getText());
+        assertTrue(messageLabel.isVisible());
+    }
+    
+    @Test
+    void propertyView_deleteAction_withoutSelection_showsMessage() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method method = PropertyView.class.getDeclaredMethod("handleDeleteProperty");
+        method.setAccessible(true);
+        method.invoke(view);
+
+        Label messageLabel = getPrivateField(view, "messageLabel", Label.class);
+
+        assertEquals("Please select a property to delete.", messageLabel.getText());
+        assertTrue(messageLabel.isVisible());
+    }
+    
+    @Test
+    void propertyView_createFormLabel_returnsStyledLabel() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method method = PropertyView.class.getDeclaredMethod("createFormLabel", String.class);
+        method.setAccessible(true);
+
+        Label label = (Label) method.invoke(view, "Address");
+
+        assertEquals("Address", label.getText());
+        assertNotNull(label.getStyle());
+        assertTrue(label.getStyle().contains("-fx-font-size"));
+    }
+    
+    @Test
+    void propertyView_createDialogField_returnsConfiguredField() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method method = PropertyView.class.getDeclaredMethod("createDialogField", String.class, String.class);
+        method.setAccessible(true);
+
+        TextField field = (TextField) method.invoke(view, "Athlone", "Enter property address");
+
+        assertEquals("Athlone", field.getText());
+        assertEquals("Enter property address", field.getPromptText());
+        assertEquals(42.0, field.getPrefHeight());
+        assertNotNull(field.getStyle());
+    }
+    
+    @Test
+    void propertyView_dialogStyleHelpers_returnExpectedStyleStrings() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method paneStyle = PropertyView.class.getDeclaredMethod("dialogPaneStyle");
+        Method primaryStyle = PropertyView.class.getDeclaredMethod("dialogPrimaryButtonStyle");
+        Method secondaryStyle = PropertyView.class.getDeclaredMethod("dialogSecondaryButtonStyle");
+
+        paneStyle.setAccessible(true);
+        primaryStyle.setAccessible(true);
+        secondaryStyle.setAccessible(true);
+
+        String pane = (String) paneStyle.invoke(view);
+        String primary = (String) primaryStyle.invoke(view);
+        String secondary = (String) secondaryStyle.invoke(view);
+
+        assertTrue(pane.contains("-fx-background-color"));
+        assertTrue(primary.contains("-fx-background-color"));
+        assertTrue(primary.contains("-fx-text-fill"));
+        assertTrue(secondary.contains("-fx-border-color"));
+    }
+    
+    @Test
+    void propertyView_helperMethods_returnExpectedValues() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(List.of());
+
+        PropertyView view = new PropertyView(fakeService);
+
+        Method safe = PropertyView.class.getDeclaredMethod("safe", String.class);
+        Method formatRole = PropertyView.class.getDeclaredMethod("formatRole", String.class);
+        Method toReference = PropertyView.class.getDeclaredMethod("toReference", String.class);
+
+        safe.setAccessible(true);
+        formatRole.setAccessible(true);
+        toReference.setAccessible(true);
+
+        assertEquals("", safe.invoke(view, new Object[]{null}));
+        assertEquals("abc", safe.invoke(view, "abc"));
+
+        assertEquals("-", formatRole.invoke(view, new Object[]{null}));
+        assertEquals("Property manager", formatRole.invoke(view, "PROPERTY_MANAGER"));
+        assertEquals("Maintenance staff", formatRole.invoke(view, "MAINTENANCE_STAFF"));
+
+        assertEquals("-", toReference.invoke(view, new Object[]{null}));
+        assertEquals("PROP-ABC", toReference.invoke(view, "abc"));
+        assertEquals("PROP-12345678", toReference.invoke(view, "123456789"));
+    }
+    
+    @Test
+    void propertyView_refreshFilterOptions_preservesValidSelection() throws Exception {
+        SessionManager.startSession("token", "manager@test.com", "PROPERTY_MANAGER");
+
+        List<Property> properties = List.of(
+                new Property("1", "Athlone", "House", "m1"),
+                new Property("2", "Dublin", "Apartment", "m2"),
+                new Property("3", "Galway", "House", "m3")
+        );
+
+        PropertyApiService fakeService = Mockito.mock(PropertyApiService.class);
+        when(fakeService.getProperties()).thenReturn(properties);
+
+        PropertyView view = new PropertyView(fakeService);
+
+        ComboBox<String> propertyTypeFilter = getPrivateField(view, "propertyTypeFilter", ComboBox.class);
+        propertyTypeFilter.setValue("House");
+
+        Method method = PropertyView.class.getDeclaredMethod("refreshFilterOptions");
+        method.setAccessible(true);
+        method.invoke(view);
+
+        assertEquals("House", propertyTypeFilter.getValue());
     }
 }
